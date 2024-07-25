6d5257d72214b5374e5151909748aa5252561c1b
Paulo <pauloh16.pdr@gmail.com>
2024-07-25 11:42:26 -0300

    feat: adicionar call-to-action para cadastro em login

--- src/components/Login/LoginForm/index.tsx
+++ src/components/Login/LoginForm/index.tsx
@@ -4,11 +4,15 @@ import React from 'react';
 
 import { useFormState, useFormStatus } from 'react-dom';
 
+import Link from 'next/link';
+
 import login from '@/actions/login';
 
 import Button from '@/components/Button';
 import Input from '@/components/Input';
 import ErrorMessage from '@/components/Helpers/ErrorMessage';
+
+import styles from './index.module.css';
 
 const SubmitButton = () => {
 	const { pending } = useFormStatus();
@@ -33,25 +37,28 @@ const LoginForm = () => {
 
 	return (
 		<>
-			<form action={action}>
-				<Input
-					label='Usuário:'
-					type='text'
-					name='username'
-					placeholder='usuário'
-				/>
+			<form className={styles.form} action={action}>
+				<Input label='Usuário:' type='text' name='username' />
 
-				<Input
-					label='Senha:'
-					type='password'
-					name='password'
-					placeholder='senha'
-				/>
+				<Input label='Senha:' type='password' name='password' />
 
 				<SubmitButton />
 
 				<ErrorMessage error={state.error} />
 			</form>
+
+			<Link href='/login/perdeu' className={styles.perdeu}>
+				Perdeu a senha?
+			</Link>
+
+			<div className={styles.cadastro}>
+				<h2 className={styles.subtitulo}>Cadastra-se</h2>
+				<p>Ainda não possui conta? Cadastre-se no site.</p>
+
+				<Link href='/login/criar' className='button'>
+					Cadastro
+				</Link>
+			</div>
 		</>
 	);
 };
