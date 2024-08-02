2739879b29a727c28e7e4364449da67ec3e3cfd4
Paulo <pauloh16.pdr@gmail.com>
2024-08-01 22:17:07 -0300

    feat: criar contexto para guardar dados de user

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,31 +2,39 @@ import type { Metadata } from 'next';
 
 import { type_second } from '@/functions/fonts';
 
+import { UserContextProvider } from '@/context/user-context';
+
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 
 import './globals.css';
+import userGet from '@/actions/user-get';
 
 export const metadata: Metadata = {
 	title: 'Dogs Next',
 	description: 'Rede social para cachorros.',
 };
 
-export default function RootLayout({
+export default async function RootLayout({
 	children,
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
+	const { data: user } = await userGet();
+
 	return (
 		<html className={type_second.variable} lang='pt-BR'>
 			<body>
-				<div className='App'>
-					<Header />
+				<UserContextProvider user={user}>
+					<div className='App'>
+						{/* @ts-expect-error Async Server Component */}
+						<Header />
 
-					<main className='AppBody'>{children}</main>
+						<main className='AppBody'>{children}</main>
 
-					<Footer />
-				</div>
+						<Footer />
+					</div>
+				</UserContextProvider>
 			</body>
 		</html>
 	);
